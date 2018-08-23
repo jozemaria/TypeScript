874726039e97633e1ae4025f59243b3951c373a7
José Maria <zeehrn@gmail.com>
2018-08-23 10:12:16 -0300

    Add files via upload
    Aula02

---
+++ ola.js
@@ -0,0 +1,13 @@
+var nome = 'José Maria';
+var data = new Date();
+
+function dia_extenso(dia) {
+    var dias = ['Domingo', 'Segunda', 'Terça', 'Quarta',
+        'Quinta', 'Sexta da Maldade', 'Sabado'];
+    // confirma se dia vai até de 0 a 6 para não quebrar
+    if (dia >= 0 && dia <= 6) {
+        return dias[dia];
+    }
+    return ''; //Retornar vazio casao seja outro dia
+}
+console.log("Ol\u00E1,  " + nome + ", hoje \u00E9 " + dia_extenso(data.getDay()));
