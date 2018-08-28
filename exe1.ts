adc2c48c502f29ebee261c0f04bca54e667ef22c
jozemaria <zeehrn@gmail.com>
2018-08-28 18:45:53 -0300

    Organização de Arquivos

---
+++ exe1.ts
@@ -0,0 +1,22 @@
+//EXERCICIO 01
+let nomes:  string[]  =  ['Bruno',  'Elaine',  'José',  'Matheus',  'Maria','Alana',  
+'Erika',  'Alison',  'Lucas',  'Rickson',  'Alberto',  'Beatriz','Renata',  'Sayonara'];
+
+//a)
+nomes.push('Paulo', 'Albertina');
+
+//b)
+console.log(`## Total de nomes ${nomes.length} ##`);
+
+//c)
+delete nomes[2];
+
+//d)
+console.log(`O nome está na ${nomes.indexOf('Bruno')+1} posição`);
+
+//e)
+
+
+//PRINT
+console.log(nomes);
+console.log(`## Total de nomes: ${nomes.length} ##`);
