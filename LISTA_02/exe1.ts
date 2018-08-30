2166da8c86a1184fb3796b49256b56751f233d36
José Maria <zeehrn@gmail.com>
2018-08-29 22:26:17 -0300

    Update exe1.ts
    Exercicio final

--- LISTA_02/exe1.ts
+++ LISTA_02/exe1.ts
@@ -3,20 +3,25 @@ let nomes:  string[]  =  ['Bruno',  'Elaine',  'José',  'Matheus',  'Maria','Al
 'Erika',  'Alison',  'Lucas',  'Rickson',  'Alberto',  'Beatriz','Renata',  'Sayonara'];
 
 //a)
-nomes.push('Paulo', 'Albertina');
+nomes.push('Paulo', 'Albertina'); //Push está recebendo duas string, e add na ultima posição.
 
 //b)
-console.log(`## Total de nomes ${nomes.length} ##`);
+console.log(`## Total de nomes ${nomes.length} ##`); //tamanho total da string
 
-//c)
-delete nomes[2];
+//c)asd
+nomes.splice(2,1); //ainda não entendi, mas tira a posição e contiua da onde tirou
+console.log(nomes);
 
 //d)
-console.log(`O nome está na ${nomes.indexOf('Bruno')+1} posição`);
+console.log(`O nome selecionado está na ${nomes.indexOf('Bruno')+1} posição`);
 
 //e)
+let cont: number = 0; //esse "for" está andando todo array, vendo os nomes que começam com A.
+for(let i in nomes){
+    if (nomes[i][0] === "A"){
+        cont += 1;
+    }
+}
 
-
-//PRINT
-console.log(nomes);
+console.log(`A quantidade de nomes com a letra A é de: ${cont}`);
 console.log(`## Total de nomes: ${nomes.length} ##`);
